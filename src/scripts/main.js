34391851a739e944f910b2baec8d36007101c683
Kateryna <katiaherascevich@gmail.com>
2025-11-23 14:55:34 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,9 +2,10 @@
 
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
-const width = wall.offsetWidth / 2 - 10 - spider.offsetWidth / 2;
-const height = wall.offsetHeight / 2 - 10 - spider.offsetHeight / 2;
+const width = wall.clientWidth / 2 - spider.offsetWidth / 2;
+const height = wall.clientHeight / 2 - spider.offsetHeight / 2;
 
 wall.style.position = 'relative';
+spider.style.position = 'absolute';
 spider.style.left = width + 'px';
 spider.style.top = height + 'px';
